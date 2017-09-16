1937f2f60e5c266c4889aec678f3cd9a29c8497a
Saurav Pratihar <sauravpratihar@Sauravs-MacBook-Air.local>
2017-09-17 03:22:32 +0530

    name&age app

--- src/App.js
+++ src/App.js
@@ -4,23 +4,41 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      name : '',
-      age : ''
-    } 
+      data : [],
+
+    }
   }
   render() {
     return (
       <div>
-        Write Your Name: <input type="text" ref="name"  onChange={this.df.bind(this)}/>
-        Write Your Age: <input type="text" ref="age"  onChange={this.df.bind(this)}/>
+        Write Your Name: <input type="text" ref="name" />
+        <br />
+        Write Your Age: <input type="text" ref="age"  />
+        <br />
+        <button onClick={this.df.bind(this)} >Add</button>
         
-        <Box name= {this.state.name} age={this.state.age} />
+        {
+          this.state.data.map((d) => (
+            <Box name= {d.name} age={d.age} key={d.name} />
+          ))
+        }
       </div>
     );
   }
 
   df(){
-    this.setState({name: this.refs.name.value, age : this.refs.age.value})
+    if(this.refs.name.value === '' || this.refs.age.value === ''){
+      alert('Insert Value');
+    }
+    else{
+      const data = {name:this.refs.name.value, age: this.refs.age.value}
+      this.state.data.push(data);
+      this.setState({data: this.state.data})
+      this.refs.name.value = ""
+      this.refs.age.value = ""
+    }
+    
+    
   }
 }
 
@@ -28,8 +46,10 @@ class Box extends Component {
   render() {
     return (
       <div>
-        <h1>Name: {this.props.name} </h1>
-        <h2>Age: {this.props.age}</h2>
+        <ul>
+          <li>Name: {this.props.name} </li>
+          <li>Age: {this.props.age}</li>
+        </ul>
       </div>
     )
   }
