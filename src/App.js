0252c18f5b688c6f7a532514513f8e25316c9a77
Saurav Pratihar <sauravpratihar@Sauravs-MacBook-Air.local>
2017-09-18 16:22:30 +0530

    2nd app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class App extends Component {
   constructor(){
     super();
     this.state = {
       data : [],
-
+      counter : 0
     }
   }
   render() {
     return (
       <div>
-        Write Your Name: <input type="text" ref="name" />
-        <br />
-        Write Your Age: <input type="text" ref="age"  />
-        <br />
-        <button onClick={this.df.bind(this)} >Add</button>
+
+        URL: <input type="text" ref="url" />
+        
+        <button onClick={this.get_json.bind(this)} >Get Data</button>
         
         {
-          this.state.data.map((d) => (
-            <Box name= {d.name} age={d.age} key={d.name} />
+          this.state.data.map((data) => (
+            <Box id= {data.id} title={data.title} body={data.body} key={data.id} />
           ))
         }
       </div>
@@ -34,11 +33,23 @@ class App extends Component {
       const data = {name:this.refs.name.value, age: this.refs.age.value}
       this.state.data.push(data);
       this.setState({data: this.state.data})
-      this.refs.name.value = ""
-      this.refs.age.value = ""
+      this.refs.name.value = "";
+      this.refs.age.value = "";
     }
+  }
+
+  get_json(){
+    axios.get(this.refs.url.value)
+    .then((response) => {
+    this.state.data.push(response.data[this.state.counter]);
+    this.setState({data: this.state.data});
+    this.setState({counter: this.state.counter + 1});
+    })
+    .catch((error) => {
+      console.log('Error occured');
+    });
     
-    
+
   }
 }
 
@@ -47,8 +58,9 @@ class Box extends Component {
     return (
       <div>
         <ul>
-          <li>Name: {this.props.name} </li>
-          <li>Age: {this.props.age}</li>
+          <li>ID: {this.props.id} </li>
+          <li>Title: {this.props.title}</li>        
+          <li>Body: {this.props.body}</li>
         </ul>
       </div>
     )
